6631bf0d030ca7d44537c89c2d9075c9d0b2f22a
pasignature <pasignature@yahoo.com>
2020-02-06 12:30:04 +0000

    modified main.c

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -5,7 +5,18 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
+int main(void){
+  int a;
+  int b;
+  int result;
+  result = add2nums(int a, int b);
+  printf("%d", result);
   return (0);
+}
+/* function to compute sum of two numbers */
+int add2nums(int a, int b){
+  int sum;
+  sum = a + b;
+  return sum;
+
 }
