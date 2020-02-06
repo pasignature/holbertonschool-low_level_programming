2a3dd82f7c6508ce20bdec6cc46d2888174dbe9d
pasignature <pasignature@yahoo.com>
2020-02-06 11:28:00 +0000

    updates on the preprocessor tasks

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-/* program entry point */
-int main(){
-  int a;
-  int b;
-  a = 2;
-  b = 3;
 
-  result = add2nums(a, b);
-
-  printf(result);
-
-}
-
-/* function that adds two numbers and returns result to main */
-int add2nums(int a, int b){
-  int sum;
-
-  sum = a + b;
-  return sum;
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  return (0);
 }
