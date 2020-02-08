2e2d01245b78e11f34529afa3e96c9778cd133de
pasignature <pasignature@yahoo.com>
2020-02-08 14:40:38 +0000

    updated  puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main -main block
- * Return:  0
+ * main - main Function
+ * Return: 0
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
