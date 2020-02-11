27bd3c7ec9a15b6defab6ed2d3b1379e2713e8c4
pasignature <pasignature@yahoo.com>
2020-02-11 21:42:42 +0000

    fixes

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @n: first integer
+ * Return: largest number
+ */
+
+positive_or_negative(n)
+{
+if (n > 0)
+  {
+printf("%d is positive\n", n);
+}
+ else if (n == 0)
+   {
+printf("%d is zero\n", n);
+}
+ else
+   {
+printf("%d is negative\n", n);
+}
+}
