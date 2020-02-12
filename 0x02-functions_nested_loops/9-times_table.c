1f60d8d43f9ffcf1820636a7a045b83f6835a50f
pasignature <pasignature@yahoo.com>
2020-02-12 17:37:53 +0000

    modified 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,19 +18,19 @@ while (a <= 9)
 b = 0;
 while (b <= 9)
 {
-if ((a * b) > 9)
+if ((a *b) > 9)
 {
-_putchar(((a * b) / 10) + '0');
-_putchar(((a * b) % 10) + '0');
+_putchar(((a *b) / 10) + '0');
+_putchar(((a *b) % 10) + '0');
 }
 else if (b != 0)
 {
 _putchar(' ');
-_putchar((a * b) + '0');
+_putchar((a *b) +'0');
 }
 else
 {
-_putchar((a * b) + '0');
+_putchar((a *b) +'0');
 }
 if (b != 9)
 {
