6c41c01b15f42bf8cb74b04ba921bb1d832f376a
pasignature <pasignature@yahoo.com>
2020-02-18 18:54:49 +0000

    uploading completed task files

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print one char out of 2 of a string
+ * @str: char array string type
+ * Description: Print 1st char, then 3rd, then 5th, etc..
+ */
+
+void puts2(char *str)
+{
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar(str[a]);
+}
+_putchar('\n');
+}
