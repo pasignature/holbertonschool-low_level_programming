315b6efaea72cab0d577465fd6747677d1f81cdd
pasignature <pasignature@yahoo.com>
2020-02-20 21:08:35 +0000

    upload 3 strcmp task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - return length of a string
+ *
+ * @s1: char type
+ * @s2: char type
+ * Return: positive negative or zero
+ */
+
+int _strcmp(char *s1, char *s2)
+int result;
+
+result = strcmp(str1, str2);
+
+result = strcmp(s1, s2);
+
+return (result);
+}
