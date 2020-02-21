3e6d42ee419b7daa59513dc6398f8df841ce2c53
pasignature <pasignature@yahoo.com>
2020-02-21 15:19:00 +0000

    modified 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,17 +15,14 @@ char *_strcat(char *dest, char *src)
 
 int i, j;
 
-/* length of s1 is stored in i */
 for (i = 0; dest[i] != '\0'; ++i)
-
-/* concatenating each character of s2 to s1 */
-for (j = 0; src[j] != '\0'; ++j, ++i)
 {
-dest[i] = src[j];
 }
-/* terminating s1 string */
+
+for (j = 0; src[j] != '\0'; ++j, ++i)
+dest[i] = src[j];
+
 dest[i] = '\0';
 
-/* returning concatenated string to main*/
 return (dest);
 }
