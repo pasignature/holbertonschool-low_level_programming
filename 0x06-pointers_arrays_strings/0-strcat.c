f1b7f2c26175b4dd34615db880924a68071261a7
pasignature <pasignature@yahoo.com>
2020-02-21 15:01:45 +0000

    modified strcart.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,14 +16,11 @@ char *_strcat(char *dest, char *src)
 int i, j;
 
 /* length of s1 is stored in i */
-for (i = 0; dest[i] != '\0'; ++i) {
-
-}
+for (i = 0; dest[i] != '\0'; ++i)
 
 /* concatenating each character of s2 to s1 */
-for (j = 0; src[j] != '\0'; ++j, ++i) {
+for (j = 0; src[j] != '\0'; ++j, ++i)
 dest[i] = src[j];
-}
 
 /* terminating s1 string */
 dest[i] = '\0';
