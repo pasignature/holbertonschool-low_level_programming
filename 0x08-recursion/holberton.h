0e681185ce02446402645d1a3eadfa2eef6d0f2c
pasignature <pasignature@yahoo.com>
2020-02-26 19:47:39 +0000

    uploading recursion mandatory tasks 1 - 7

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,23 @@
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int _sqrt_recursion(int n);
+
+int is_prime_number(int n);
+
+int is_palindrome(char *s);
+
+int if_natural_square(int a, int b);
+
+int check_prime(int a, int b);
+
+int charcheck(char *str, int length);
