80dd7d52eae73d9573d564be91bff6ebfc83edaf
pasignature <pasignature@yahoo.com>
2020-03-05 22:21:49 +0000

    modified 100 realoc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,18 +15,25 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *s;
 
 	if (new_size > old_size)
+	{
 		s = malloc(new_size);
-	free(ptr);
-	return (s);
+		free(ptr);
+		return (s);
+	}
 	if (new_size == old_size)
+	{
 		return (ptr);
+	}
 	if (ptr == NULL)
+	{
 		s = malloc(new_size);
-	free(ptr);
-	return (s);
+		free(ptr);
+		return (s);
+	}
 	if (new_size == 0 && ptr != NULL)
-	free(ptr);
-return (NULL);
-
-return (ptr);
+	{
+		free(ptr);
+		return (NULL);
+	}
+	return (ptr);
 }
