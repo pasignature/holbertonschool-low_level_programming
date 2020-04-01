2afede3a3a5fe2081fc2916e5f7e61dd522664cd
pasignature <pasignature@gmail.com>
2020-04-01 13:15:54 +0000

    modified 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,3 @@
-#include <fcntl.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include "holberton.h"
 
 /**
@@ -20,43 +15,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r, w;
 	char *buf;
 
-	if (!filename)
+	fd = open(filename, O_RDONLY);/* opens file with rd + wr perms */
+	buf = malloc(letters * sizeof(char));/* allocates buffer */
+	if (!filename || fd == -1 || !buf)
 		return (0);
-
-	/* opens file with read + write permissions and checks for error */
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-
-	/* allocates buffer of size letters and checks for error */
-	buf = malloc(letters * sizeof(char));
-	if (!buf)
-		return (0);
-
-	/* reads text into buffer from file and checks for error */
-	r = read(fd, buf, letters);
-	if (r == -1)
-	{
-		free(buf);
-		return (0);
-	}
-
-	close(fd);
-
-	/* write to stdout from buffer and checks for error */
-	w = write(STDOUT_FILENO, buf, r);
-	if (w == -1)
-	{
-		free(buf);
-		return (0);
-	}
-
-	/* checks if write wrote the expected amount of bytes */
-	if (w < r)
+	r = read(fd, buf, letters);/* reads text into buffer */
+	w = write(STDOUT_FILENO, buf, r);/* write to stdout from buffer */
+	if (r == -1 || w == -1 || w < r)/* checks if wr wrote expected bytes */
 	{
 		free(buf);
 		return (0);
 	}
 	free(buf);
+	close(fd);
 	return (w);
 }
