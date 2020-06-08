168259721343be1bcfc791bec4df7c816b51e0c4
pasignature <pasignature@gmail.com>
2020-06-08 17:56:32 +0000

    updates

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,7 +6,7 @@
  * @size: size of the array of the hash table
  * Return: index of key
  */
- 
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
