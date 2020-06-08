bb61ce8911a2e2bbeb36bc66772ec100a8a32c4f
pasignature <pasignature@gmail.com>
2020-06-08 17:39:32 +0000

    updates

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,22 +9,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-  hash_table_t *h_table = NULL;
-  hash_node_t **array = NULL;
-  unsigned long int count;
+	hash_table_t *h_table = NULL;
+	hash_node_t **array = NULL;
+	unsigned long int count;
 
-  h_table = malloc(sizeof(hash_table_t));
-  if (!h_table)
-    return (NULL);
+	h_table = malloc(sizeof(hash_table_t));
+	if (!h_table)
+		return (NULL);
 
-  h_table->size = size;
-  array = malloc(sizeof(hash_node_t *) * size);
-  if (!array)
-    return (NULL);
+	h_table->size = size;
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (!array)
+		return (NULL);
 
-  for (count = 0; count < size; count++)
-    array[count] = NULL;
+	for (count = 0; count < size; count++)
+		array[count] = NULL;
 
-  h_table->array = array;
-  return (h_table);
+	h_table->array = array;
+	return (h_table);
 }
