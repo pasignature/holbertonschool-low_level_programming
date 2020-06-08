026749b84bcec611d0857e4e4c524da8c98b75b8
pasignature <pasignature@gmail.com>
2020-06-08 17:41:53 +0000

    updates

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,18 +1,19 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - generates hash for a key
- * @str: key
- * Return: hash
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int c;
+	int count;
 
 	hash = 5381;
-	while ((c = *str++))
+	while ((count = *str++))
 	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+		hash = ((hash << 5) + hash) + count; /* hash * 33 + c */
 	}
 	return (hash);
 }
