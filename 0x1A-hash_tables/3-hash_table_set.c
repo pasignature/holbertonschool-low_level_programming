0fefefa025da0b7b0c750e238f9022a2dab0aece
pasignature <pasignature@gmail.com>
2020-06-08 18:03:48 +0000

    updates

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,28 +3,28 @@
 #include <stdlib.h>
 
 /**
- * hash_table_set - adds a new value to the hash table
- * @ht: hash table
- * @key: key
- * @value: value
- * Return: 1 on success, 0 if failed
+ * hash_table_set - function that adds an element to a hash table
+ * @ht: is the hash table to be updated
+ * @key: key cannot be empty
+ * @value: value to be associated to the key
+ * Return: 1 if successful, 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node = 0, *idx_node = 0;
+	hash_node_t *new_node = 0, *indx_node = 0;
 
-	if (!strlen(key) || !ht)
+	if (!ht || !ht->array || !key || !value || strlen(key) == 0)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	indx_node = (ht->array)[index];
 
-	idx_node = (ht->array)[index];
-
-	if (idx_node && !strcmp(idx_node->key, key))
+	if (indx_node && !strcmp(indx_node->key, key))
 	{
-		free(idx_node->value);
-		idx_node->value = strdup(value);
+		free(indx_node->value);
+		indx_node->value = strdup(value);
 		return (1);
 	}
 
@@ -34,8 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-
 	(ht->array)[index] = new_node;
-	new_node->next = idx_node;
+	new_node->next = indx_node;
 	return (1);
 }
