3e87071963942de296fc0453d5fb0e10162e4a7b
pasignature <pasignature@gmail.com>
2020-06-08 17:32:09 +0000

    updates

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,9 +9,13 @@
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-  unsigned long int hash = 5381;
-  int count;
-  while ((count = *str++))
-    hash = ((hash << 5) + hash) + count; /*hash * 33 + count*/
+  unsigned long int hash;
+  int c;
+
+  hash = 5381;
+  while ((c = *str++))
+    {
+      hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
   return (hash);
 }
