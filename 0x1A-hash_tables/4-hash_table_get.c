bd55a0b071bcef3946ae7d17fd361b9f26ccafc9
pasignature <pasignature@gmail.com>
2020-06-08 18:11:18 +0000

    updates

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,14 +13,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int arr_index;
 	hash_node_t *ht_node = NULL;
 
-	if (!ht_node)
-		return (NULL);
 	if (!ht || !ht->array || !key || strlen(key) == 0)
 		return (NULL);
 
 	arr_index = key_index((const unsigned char *)key, ht->size);
 	ht_node = (ht->array)[arr_index];
-
+	if (!ht_node)
+		return (NULL);
+	
 	while (ht_node)
 	{
 		if (!strcmp(ht_node->key, key))
